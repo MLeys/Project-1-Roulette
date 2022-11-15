4a15f3db614c48d6ee054778443a83bf1b297873
username <mleyspainting@gmail.com>
2022-11-15 00:06:20 -0500

    WIP - event listeners

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ const NUMBERS = ['0','00',
                 '1','2','3','4','5','6','7','8','9','10',
                 '11','12','13','14','15','16','17','18','19','20',
                 '21','22','23','24','25','26','27','28','29','30',
-                '31','32','33','34','35','36']
+                '31','32','33','34','35','36'];
 
 const SIDEBETS = ['1-12','13-24','25-26',
                 '18-','19+', 'even', 'odd', 'red', 'black',
@@ -14,8 +14,7 @@ const SIDEBETS = ['1-12','13-24','25-26',
 const PAYOUTS =[
     {type: 'num', payout: 35},
     {type: 'fiftyFifty', payout: 1},
-    {type: 'thirds', payout: 2}
-];
+    {type: 'thirds', payout: 2}];
 
 
 
@@ -41,19 +40,58 @@ let winningNum; // Random number generated to represent result of wheel spin
 
 
 /*----- cached element references -----*/
-const chipEl = document.querySelectorAll('.chip');
+
+// const chipEl = document.querySelectorAll('.chip');
 const spinEl = document.querySelector('#spin');
 
-const even = document.querySelector('#bet-even');
-const odd = document.querySelector('#bet-odd');
-const black = document.querySelector('#bet-black');
-const red = document.querySelector('#bet-red');
+//testing borrowed code
+const evenBtn = document.querySelectorAll('.even');
+evenBtn.forEach(bet => {
+    bet.addEventListener('click', function placeBet(){
+        
+
+    })
+});
+
+
+const numsEl = document.querySelectorAll(".num");
+numsEl.addEventListener('click', function(e){
+    console.log(e.target.id);
+    placeBet;
+
+});
+
+
+
+
+
+
+
+
+
+// evenBtn.addEventListener('click', placeBet);
+const evenNums = document.querySelectorAll('.even');
+console.log(evenNums.length)
+
+
+const oddNums = document.querySelector('#odd');
+const blackHalf = document.querySelector('#black');
+const redHalf = document.querySelector('#red');
 const firstHalf = document.querySelector('#bet-1-18');
 const secondHalf = document.querySelector('#bet-19-36');
+
 
 const num1 = document.querySelector('#bet-1');
 
 /*----- event listeners -----*/
+
+// evenNums.addEventListener('click', placeBet);
+// oddEl.addEventListener('click', placeBet);
+// blackEl.addEventListener('click', placeBet);
+// redEl.addEventListener('click', placeBet);
+// firstHalfEl.addEventListener('click', placeBet);
+// secondHalfEl.addEventListener('click', placeBet);
+
 
 // const chipSelect = addEventListener('click', selectChip);
 
@@ -67,8 +105,9 @@ function availCredit() {
 
 
 function placeBet(e) {
+
     let betObject ={id: e.target.id, wager: CHIP[0]};    // ^^ change chip val
-    (e.target.id != '' ? pBets.push(betObject) : null)
+    (e.target.id != '' ? pBets.push(betObject) && (pCredit -= betObject['wager']) : null)
     pCredit -= betObject['wager'];
     console.log(pCredit + "<--pCREDIT")
 }
@@ -76,7 +115,6 @@ function placeBet(e) {
 function init() {
     pBets = [];
     pCredit = 100;
-
 }
 
 function spinWheel() {
