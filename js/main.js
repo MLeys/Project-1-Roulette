eacb246574c810d3bb40c73107cb60bff90b3a94
username <mleyspainting@gmail.com>
2022-11-17 22:10:55 -0500

    Cleaned JS file

--- js/main.js
+++ js/main.js
@@ -103,8 +103,6 @@ const closeBtnWinnings = document.getElementsByClassName("winningsExit");
 
 
 
-
-
 /*----- event listeners -----*/
 tableAreaEl.addEventListener('click', placeBet);
 spinBtnEl.addEventListener('click', spinWheel)
@@ -112,87 +110,54 @@ chipBtn.addEventListener('click', selectChip)
 resetBtn.addEventListener('click', resetBets);
 
 
-
-
 init();
-console.log(`^^^^^ WAGER START: $${pWager} ^^^^^`)
 /*----- functions -----*/
 wheelSpinMessage.innerHTML = (`Num: ${winningNum} WIN:${pWinnings}`);
-
-
-
 
 function init() {
     clearTable();
     rmActiveClassAllBtns();
 
-    
     pCredit = 100;
     pTotalBet = 0;
     winningNum = +'';  // TEMP VALUE FOR TESTING
 
-
-    
     render();
 }
 
 function resetBets(){
-    console.log(`------CLEARING BETS--------`);
-    console.log(pBets);
-    console.log(pTotalBet);
-    console.log(pCredit);
-    console.log(`^^^^^^^^ pBets ARRAY ^^^^^^^`);
     pCredit += +pTotalBet;
-    console.log(`NEW CERDIT:   ${pCredit}`);
     clearTable();
 }
 
 function selectChip(e) {
-    rmAllClassActive();
-    
-
+    rmClassActiveChips();
     e.target.classList.add('active');
     let chipId = e.target.id
 
-    // REMOVE ALL active CLASS
     if (e.target.id.includes('chip')) {
-        console.log(`-- CHIP SELECTED: ${e.target.id.replace('chip', '')}`);
         pWager = e.target.id.replace('chip', '');
     } 
-
-    console.log(`***** CHIP SELECTED: $${pWager}-----*****`);
     render();
 }
 
 function placeBet(e) {
-    console.log("------placeBet------")
-    console.log(`-----$${pWager} per click-----`)
-    console.log(`---$${pTotalBet} Total ALL bets-----`)
     e.target.classList.add('active');
 
     let betId = e.target.id;
-    
     let betObject ={id: betId, wager: pWager};
     console.log(betObject);    
-
 
     NUMBERS.includes(betId) || SIDEBETS.includes(betId)
     ? (pBets.push(betObject)) 
         && (pCredit -= betObject['wager'])
         && addBetToTotal(pWager)      
     : null;
-    
-    console.log(pBets, "<--pBets Array");
-    console.log(pCredit, "<-------pCredit Available");
 
-    
     render();
 }
 
-
-
-
-function rmAllClassActive() {
+function rmClassActiveChips() {
     allChipButtons.forEach((element) => {
         element.classList.remove('active');
         
@@ -201,9 +166,6 @@ function rmAllClassActive() {
 }
 
 function rmActiveClassAllBtns () {
-    // allChipButtons.forEach((element)=> {
-    //     element.classList.remove('active');
-    // })
     allFiftyFiftyButtons.forEach((element)=> {
         element.classList.remove('active');
     })
@@ -217,12 +179,8 @@ function rmActiveClassAllBtns () {
 }
 
 function addBetToTotal(e) {
-    // console.log(pTotalBet, "<--previous total");
-
     pTotalBet = (pTotalBet*1) + (e*1);
-    // console.log(pTotalBet, "<--- NEW total")
     render()
-
 }
 
 function clearTable() {
@@ -243,12 +201,7 @@ function findWinningBets() {
     render();
 }
 
-function pBetWon() {
-
-}
-
 function spinWheel() {
-
     winningNum = Math.floor(Math.random() * NUMBERS.length); // TEMP DISABLED
     render();
     console.log(winningNum, '<-- Winning Number!')
@@ -264,166 +217,30 @@ function spinWheel() {
 
             for (let j = 0; j < PAYOUTS.length; j++) {
                 if (PAYOUTS[j]['betType'].includes(checkBet) ) { 
-                    // pWinningBets.push({bet: checkBet, betWager})
-
                     pWinnings = +pWinnings + ((PAYOUTS[j]['payout']) * (+betWager));
- 
                     pCredit = +pCredit +pWinnings + +betWager;
                 }
-
             }
-            
-        } else {
-            console.log(`\\\\\\\\\\ not paid //////////`)
-            
-        }
-
-            }
-    
-    console.log(`++++++ Player Credit:   $${pCredit}   +++++++`)
-    console.log(`\\\\\\\\\\ END LOOPS //////////`)
+        } 
+    }
     clearTable();
     rmActiveClassAllBtns();
     render();
-
-}
+};
 
 spinBtn.onclick = function() {
     spinModal.style.display = "block";   
 }
-
 closeBtnSpin.onclick = function () {
     spinModal.style.display = 'none';
-    
-
-}
-closeBtnWinnings.onclick = function () { 
-    winningsModal.style.display = 'none';
-
 }
 tableAreaEl.onclick = function(e) {
     e.target == spinBtnEl ? spinModal.style.display ="none" :null;
 }
-
-
-
 
 function render() {
     totalEl.innerHTML = (`$ ${pCredit}`);
     totalBetEl.innerHTML = (`$ ${pTotalBet}`);
     
     wheelSpinMessage.innerHTML = winningNum;
-    
 }
-
-// const spinModal = document.getElementById("mySpinModal");
-// const spinBtn = document.getElementById("spin");
-// const closeBtnSpin = document.getElementsByClassName("close")[0];
-
-// for (let j = 0; j < PAYOUTS.length; j++) {
-//     if (PAYWinnings[j]['betType'].includes(checkBet) ) { 
-//         console.log(`BET PAYS: $${betWager} * ${PAYOUTS[j]['payout']} = ${betWager*PAYOUTS[j]['payout']}`);
-//         pWinnings = +pWinnings + ((PAYOUTS[j]['payout']) * (+betWager));
-//         console.log('---------WINNINGS------------');
-//         console.log(`----------${pWinnings}----------`);
-//         console.log(`-------------------------------`);   
-//         pCredit = +pCredit +pWinnings + +betWager;
-//     }
-
-
-// function placeBet(e) {
-//     e.target.classList.add('active');
-//     let betId = e.target.id;
-//     let betObject;
-//     let betWager = pWager;
-       
-// //checking to make sure where clicked is a bet button
-//     if (NUMBERS.includes(betId) || SIDEBETS.includes(betId) ) {
-//         betObject ={id: betId, wager: betWager};
-//         console.log(betObject); 
-
-//         for (let k = 0; k < pBets.length ; k++) {
-//             let checkBetObject = pBets[k];
-//             console.log(`pBets[${k}] value <---of ${pBets.length} ------`);
-            
-            
-            
-//             if (checkBetObject['id'] === betObject['id']) {
-//                 console.log(checkBetObject['id'] === betObject['id'], '---????');
-//                 console.log(`ID: ${checkBetObject['id']}  BEFORE wager: ${checkBetObject['id']}`);
-//                 checkBetObject['wager'] = +checkBetObject['wager'] + +betWager;
-//                 console.log(`AFTER wager: ${checkBetObject['wager']}`);
-//                 console.log(`NEW pBets: ${pBets}) !!!!!!!!`); 
-//                 break;
-
-//             } else {
-                
-//                 (k === pBets.length -1 ? pBets.push(betObject)&& console.log(` ^^^^^^ ADDING OBJ TO LIST ^^^^^`) : console.log('nothing') );
-                
-//             }
-//         }
-
-//         (pBets.length === 0 ? pBets.push(betObject): null);
-
-        
-//         pCredit -= betObject['wager'];
-//         addBetToTotal(pWager);
-//     }
-
-            
-//     console.log(pBets, "<--pBets Array");
-//     console.log(pCredit, "<-------pCredit Available");
-//     render();
-// } 
-
-               
-        
-
-        // if (pBets.length !== 0) {
-        //     for (let i = 0; i <pBets.length; i++) {
-        //         let checkBet = pBets[i];
-
-        //         for (let id in checkBet) {
-        //             if (checkBet[id] === betObject['id']) {
-        //                 console.log(` TRUE-->  ${checkBet[id]} === ${betObject['id']}`);
-        //                 break;
-
-                    
-        //             } else { 
-        //                 console.log(` FALSE-->  ${checkBet[id]} === ${betObject['id']}`)};
-        //                 pBets.push(betObject)
-        //                 break;
-
-        //         };
-
-                // console.log(`CHECKING: ${i} index is ${checkBet}`);
-                // console.log(`betOject is: --- ${betObject['id']}`);
-                // console.log(`${pBets[i]['id'].includes(betId)} < ---- TRUE or FALSE`)
-                
-                // if (pBets[i]['id'].includes(betObject['id']) ) {
-                //     console.log(`WSDFDSAKJASDKJDSA:JFA:LFJDSL:FAS:KSD":KS`);
-
-
-
-                // //     console.log(`wager before: bet:${pBets[i]['id']} wager: ${pBets[i]['wager']}`);
-                // //     pBets[i]['wager'] += betObject['wager'];
-                // //     console.log(`wager after: bet:${pBets[i]['id']} wager: ${pBets[i]['wager']}`);
-                // // } else {
-                //     break;
-                // } else {
-                //     pBets.push(betObject);
-                // };
-            // }
-
-
-        
-
-//         pCredit -= betObject['wager'];
-//         addBetToTotal(pWager);
-//     }
-
-            
-//     console.log(pBets, "<--pBets Array");
-//     console.log(pCredit, "<-------pCredit Available");
-//     render();
-// } 
